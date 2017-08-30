f65f30cc4fd5c41e14bfef716fe652904993d53f
Collins <arkucollins@gmail.com>
2017-08-30 13:53:22 +0000

    home page complete

---
+++ Home/resources/views/App/tr.blade.php
@@ -0,0 +1,55 @@
+<div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+       <h2 id="skills">Skills</h2>
+          <hr>
+          <li>Possess effective communication skills, which enable me to get the point across in a way in which technologically savvy individuals and laypersons can understand.</li>
+<li>Possess a well-rounded computer education and work history, which enables me to work in a variety of job roles related to Information Technology.</li>
+<li>Perform various researching duties in an effective and efficient manner in such a way to leave no document unturned.</li>
+       <li> Ability to creatively develop new computer programs which will touch upon various pertinent aspects necessary in today’s society. </li>
+       </div>
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+       <h2 id="skills">Landguage</h2>
+          <hr>
+          	<section class="">
+				<p>
+          
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Logic Building</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">C++</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">C#</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">JAVA (J2EE)</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">PHP</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">PHP</a>
+				</p>
+      </section>
+        <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+              </h2>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 18, 2017</p>
+          </div>
+           <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+              </h2>
+            </a>
+            <p class="post-meta">Posted by
+              <a href="#">Start Bootstrap</a>
+              on September 18, 2017</p>
+          </div>
+      
+          </div>
+          
+      </div>
--- Home/resources/views/Pages/index.blade.php
+++ Home/resources/views/Pages/index.blade.php
@@ -12,6 +12,8 @@
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+       <link href="css/component.css" rel="stylesheet">
+        <link href="css/default.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -20,13 +22,14 @@
 
     <!-- Custom styles for this template -->
     <link href="css/clean-blog.min.css" rel="stylesheet">
+    <script src="js/modernizr.custom.js"></script>
 
   </head>
 
   <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+ <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
       <div class="container">
         <a class="navbar-brand" href="index.html">Arku Collins Yao</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -74,14 +77,100 @@ a bit about me:
       </div>
     </header>
 
-    <!-- Main Content -->
+   <!-- Main Content -->
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-preview " >
+            <a href="#">
+              <h2 class="post-title">
+                Skills
+              </h2>
+              </a>
+            <li>Possess effective communication skills, which enable me to get the point across in a way in which technologically savvy individuals and laypersons can understand.</li>
+        <li>Possess a well-rounded computer education and work history, which enables me to work in a variety of job roles related to Information Technology.</li>
+        <li>Perform various researching duties in an effective and efficient manner in such a way to leave no document unturned.</li>
+        <li> Ability to creatively develop new computer programs which will touch upon various pertinent aspects necessary in today’s society. </li> 
+        </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+               Language
+              </h2>
+            </a>
+            <section class="">
+				<p>
           
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Logic Building</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">C++</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">C#</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">JAVA (J2EE)</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">PHP</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">PHP</a>
+				</p>
+      </section>
           </div>
           <hr>
-        
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+               Framework
+              </h2>
+            </a>
+            <section class="">
+				<p>
+          
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Laravel</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Bootstrap</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Bootface</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Django</a>
+       
+				</p>
+      </section>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+               Infrastructure
+              </h2>
+            </a>
+            <section class="">
+				<p>
+          
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Linux</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Mysql</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">sql</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">lamp server</a>
+       
+				</p>
+      </section>
+          </div>
+          <hr>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+               Tools
+              </h2>
+            </a>
+            <section class="">
+				<p>
+          
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Netbeans</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Visual Studio code</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">git</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Adobe photoshop</a>
+        <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">font awesome</a>
+         <a href="/c" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;">Adobe photoshop</a>
+        </p>
+      </section>
+          </div>
+          <hr>
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-secondary float-right" href="#">top</a>
+          </div>
         </div>
       </div>
     </div>
@@ -125,13 +214,14 @@ a bit about me:
       </div>
     </footer>
 
+
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/popper/popper.min.js"></script>
     <script src=" vendor/bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- Custom scripts for this template -->
-    <script src="js/clean-blog.min.js"></script>
+   
+
 
   </body>
 
