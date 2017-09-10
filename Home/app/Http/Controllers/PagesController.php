82f5533c783b7c2f5bed92481d2dfd687d9a238e
Collins <arkucollins@gmail.com>
2017-09-10 17:10:32 +0000

    mail to refs #3 is fixed

--- Home/app/Http/Controllers/PagesController.php
+++ Home/app/Http/Controllers/PagesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-//use Illuminate\Http\Request;
+use App\Http\Requests;
 use Mail;
+use Session;
 use App\Http\Controllers\Controller;
 
 class PagesController extends Controller {
@@ -31,33 +32,33 @@ class PagesController extends Controller {
     }
     public function getinfo(){
          
-         return view('Page.contact');
+         return view('Pages.contact');
     }
     public function postContacts(Request $request){
         $this->validate($request,[
+            'name' =>'required',
            'email' =>'required|email',
-           'name' =>'required',
            'phone' =>'required',
            'message' =>'required'
 
         ]);
 
             $data = array(
-            'email' => $request->email,
             'name' => $request->name,
-            'message'=>$request->message,
-            'phone' =>$request->phone
+            'email' => $request->email,
+            'phone'=>$request->phone,
+            'bodyMessage' =>$request->message
             );
 
         Mail::send('email.contact', $data, function($messages) use ($data){
             $messages->from($data['email']);
             $messages->to('arkucollins@gmai.com');
-            $messages->subject($data['name']);
 
             
-           
-
          });
+       //Session::flash('success','Your email was Sent');
+       //return redirect()->url('/');
+        return redirect()->back()->with('error', 'Something went wrong.');
     }
 
 } 
--- Home/resources/views/email/contact.blade.php
+++ Home/resources/views/email/contact.blade.php
@@ -1,1 +1,9 @@
- 
+<!doctype thml>
+<html>
+<head></head>
+<body style="background: black; color: white">
+<h1>{{$name}}</h1>
+<p>{{$phone}}</p>
+<p>{{$bodyMessage}}</p>
+</body>
+</html>
