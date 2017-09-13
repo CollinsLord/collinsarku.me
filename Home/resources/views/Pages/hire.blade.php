bf018c4dc22e0bac9a539d77c156547811784d44
Collins <arkucollins@gmail.com>
2017-09-13 10:49:59 +0000

    completed hire page

--- Home/resources/views/Pages/hire.blade.php
+++ Home/resources/views/Pages/hire.blade.php
@@ -20,13 +20,13 @@
                 Hello
               </h2>
               </a>
-              I am a developer with experience in my field.I currently leave in Accra Ghana,
-            I have built several projects on my own from scratch and work with other developers on projects.
-              I think my experience in the industry and my ability to work autonomously 
-              make me a good developer to work as an individual or within a team.
+              I am a developer based in Accra Ghana,with experience in Web designing and Application development, 
+              I have built several projects on my own from scratch and work with other developers on some few projects.
+              <p>I think my experience in the industry and my ability to work autonomously 
+              make me a good developer to work as an individual or within a team.</p>
               I have the savvy, experience and superior communication ability to be an asset to you as well as your company.
-              Please feel free to checkout my projects  I have worked on <a href="#">Github</a> or on my project page
-              If you have a project you wish to discuss or work with me you can get in touch with me on my <a href="/contact">Contact me </a>
+              Please feel free to checkout my projects  I have worked on <a href="https://github.com/CollinsLord" style="color:Blue">Github</a> or on my project page.
+              <p>If you have a project you wish to discuss or work with me,kindly contact me on my <a href="/contact" style="color:Blue">Contact Page</a></p>
             
         </div>
           <hr>
