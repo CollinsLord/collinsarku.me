57ac1fd5c648e311f4d8ccba33ede1bb228842b4
Collins <arkucollins@gmail.com>
2017-09-14 17:20:09 +0000

    working on the project page

--- Home/resources/views/Pages/project.blade.php
+++ Home/resources/views/Pages/project.blade.php
@@ -11,19 +11,29 @@
 
 
 @section('body')
-<div class="post-preview " >
-            <a href="#">
+
+          <div class="post-preview">
+            <a href="post.html">
               <h2 class="post-title">
-                Skills
+               Selected Project
               </h2>
-              </a>
-            <li>Possess effective communication skills, which enable me to get the point across in a way in which technologically savvy individuals and laypersons can understand.</li>
-        <li>Possess a well-rounded computer education and work history, which enables me to work in a variety of job roles related to Information Technology.</li>
-        <li>Perform various researching duties in an effective and efficient manner in such a way to leave no document unturned.</li>
-        <li> Ability to creatively develop new computer programs which will touch upon various pertinent aspects necessary in today’s society. </li> 
-        </div>
+            </a>
+            <section class="">
+				<p>
           
-          <hr>
+         <a  data-toggle="collapse" href="#collins" aria-expanded="false" aria-controls="collapseExample" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;color:white">collinsarku.me</a>
+         <a href="http://pricomghana.com" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;color:white">Pricomghana.com</a>
+         <a href="http://clickitconsult.com" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;color:white">Clicksdconsult.tech</a>
+         <a href="http://aptechghana.com" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;color:white">Aptechghana.com</a>
+         <a href="http://glazicogh.com" class="btn btn-1 btn-1a color-1" role="button" style="margin-right: -30px;color:white">Glazicoghana.com</a>
+        </p>
+        <div class="collapse" id="collins">
+  <div class="card card-body">
+    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+  </div>
+</div>
+      </section>
+          </div>
           
           <hr>
           <!-- Pager -->
